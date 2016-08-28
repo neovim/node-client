e06c4b3cae16deac161bf1dc325e5dc394d9c375
Billy Vong <billy@opstarts.com>
2016-08-28 09:37:43 -0700

    Add promise API

---
+++ promise.js
@@ -0,0 +1,52 @@
+/* jshint loopfunc: true, evil: true */
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+var traverse = require('traverse');
+var Session = require('msgpack5rpc');
+var _ = require('lodash');
+var attachWithCallback = require('.').default;
+
+module.exports = function attach(writer, reader) {
+  return new Promise(function(resolve, reject) {
+    attachWithCallback(writer, reader, function(err, nvim) {
+      if (err) {
+        reject(err);
+      } else {
+        //promisify APIs
+        var interfaces = {
+          Nvim: nvim.constructor,
+          Buffer: nvim.Buffer,
+          Window: nvim.Window,
+          Tabpage: nvim.Tabpage,
+        };
+
+        Object.keys(interfaces).forEach(function(key) {
+          var name = key;
+          Object.keys(interfaces[key].prototype).forEach(function(method) {
+            const oldMethod = interfaces[key].prototype[method];
+            interfaces[key].prototype[method] = function() {
+              const args = Array.prototype.slice.call(arguments);
+              const context = this;
+              return new Promise(function(resolve, reject) {
+                args.push(function(err, result) {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    resolve(result);
+                  }
+                });
+                oldMethod.apply(context, args);
+              });
+            };
+          })
+        });
+
+        resolve(nvim);
+      }
+    });
+  });
+}
+
+// 'default' export for ES2015 or TypeScript environment.
+module.exports.default = module.exports;
