1e255678151abd3fde9b80b5387740f7da97e4e4
Billy Vong <billy@opstarts.com>
2017-06-10 20:33:54 -0700

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ]
+  // extending prettier fucks up my vim...
+  extends: ['airbnb-base'],
+  plugins: ['import'],
+  rules: {
+    'class-methods-use-this': 0,
+    'comma-dangle': ["error", {
+      arrays: "only-multiline",
+      objects: "only-multiline",
+      imports: "only-multiline",
+      exports: "only-multiline",
+      functions: "ignore",
+    }],
+    'no-underscore-dangle': 0,
+    'no-mixed-operators': 0,
+    'func-names': 0,
+
+    // prettier things
+    'arrow-parens': 0,
+    'space-before-function-paren': 0,
+  },
 };
