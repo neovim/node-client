09cf275a5beb4b7f7a6a5cb9b3e9cf6f1290956d
Billy Vong <billy@opstarts.com>
2017-06-11 19:08:21 -0700

    Fix generated API methods
    * "methods" were not sending `this` as first argument
    * request callback was decoding response, was breaking calls that passed
    an ext type as first argument

--- src/api/helpers/generateWrappers.js
+++ src/api/helpers/generateWrappers.js
@@ -1,28 +1,21 @@
 const parseFunctionMetadata = require('./parseFunctionMetadata');
 
-const createApiMethod = function({ name, takesCallback }) {
-  let newMethod;
-  if (takesCallback) {
-    newMethod = function(...args) {
-      this.logger.debug(`request -> neovim.api.${name}`);
-      return new Promise((resolve, reject) => {
-        // does args need this?
-        this._session.request(name, args, (err, res) => {
-          this.logger.debug(`neovim.api.${name}.resp: ${res}`);
-          if (err) {
-            reject(new Error(`${name}: ${err[1]}`));
-          } else {
-            resolve(this._decode(res));
-          }
-        });
+const createApiMethod = function({ name }) {
+  const newMethod = function(...args) {
+    this.logger.debug(`request -> neovim.api.${name}`);
+    return new Promise((resolve, reject) => {
+      // does args need this?
+      this._session.request(name, args, (err, res) => {
+        this.logger.debug(`neovim.api.${name}.resp: ${res}`);
+        if (err) {
+          reject(new Error(`${name}: ${err[1]}`));
+        } else {
+          // No need to decode response
+          resolve(res);
+        }
       });
-    };
-  } else {
-    newMethod = function(...args) {
-      this.logger.debug(`notify -> neovim.api.${name}`);
-      this._session.notify(name, args);
-    };
-  }
+    });
+  };
 
   Object.defineProperty(newMethod, 'name', { value: name });
   return newMethod;
@@ -81,7 +74,9 @@ function generateWrappers(cls, types, prefixMap, metadata) {
     // In that case using a Proxy would be better so can we catch all undefined api calls
     // But this is fine for now
     Type.prototype[methodName] = function(...a) {
-      return method.apply(this, a);
+      // eslint-disable-next-line no-shadow
+      const args = isMethod ? [this, ...a] : a;
+      return method.apply(this, args);
     };
 
     Object.defineProperty(Type.prototype[methodName], 'metadata', {
