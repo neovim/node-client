9eb501587c9b30dfaacaf5bb350a5cf1bd18dba0
Billy Vong <billy@opstarts.com>
2017-06-12 18:59:36 -0700

    Change host to respond to `--version` request (for CheckHealth)

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,14 @@ const logger = require('../src/logger');
 // node <current script> <rest of args>
 const [, , ...args] = process.argv;
 
+if (args[0] === '--version') {
+  // eslint-disable-next-line global-require
+  const pkg = require('../package.json');
+  // eslint-disable-next-line no-console
+  console.log(pkg.version);
+  process.exit(0);
+}
+
 try {
   const host = new Host(args);
   host.start({ proc: process });
