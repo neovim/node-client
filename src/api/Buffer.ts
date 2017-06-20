5a499b8058546d3f9b3cdf541704749dafe649ea
Billy Vong <billyvg@gmail.com>
2017-06-20 06:23:02 -0700

    Add types to Buffer

--- src/api/Buffer.ts
+++ src/api/Buffer.ts
@@ -1,22 +1,22 @@
-import { BaseApi } from "./Base";
+import { BaseApi } from './Base';
 export interface BufferSetLines {
-  start?: number;
-  end?: number;
-  strictIndexing?: boolean;
+  start?: number,
+  end?: number,
+  strictIndexing?: boolean,
 }
 export interface BufferHighlight {
-  hlGroup?: string;
-  line?: number;
-  colStart?: number;
-  colEnd?: number;
-  srcId?: number;
-  async?: boolean;
+  hlGroup?: string,
+  line?: number,
+  colStart?: number,
+  colEnd?: number,
+  srcId?: number,
+  async?: boolean,
 }
 export interface BufferClearHighligh {
-  srcId?: number;
-  lineStart?: number;
-  lineEnd?: number;
-  async?: boolean;
+  srcId?: number,
+  lineStart?: number,
+  lineEnd?: number,
+  async?: boolean,
 }
 export class Buffer extends BaseApi {
   get length(): Promise<number> {
@@ -37,62 +37,62 @@ export class Buffer extends BaseApi {
   getLines(
     { start, end, strictIndexing } = { start: 0, end: -1, strictIndexing: true }
   ): Promise<Array<string>> {
-    const indexing = typeof strictIndexing === "undefined"
+    const indexing = typeof strictIndexing === 'undefined'
       ? true
       : strictIndexing;
     return this.request(`${this.prefix}get_lines`, [
       this,
       start,
       end,
-      indexing
+      indexing,
     ]);
   }
 
   setLines(
     _lines: string | string[],
     { start: _start, end: _end, strictIndexing }: BufferSetLines = {
-      strictIndexing: true
+      strictIndexing: true,
     }
   ) {
     // TODO: Error checking
     // if (typeof start === 'undefined' || typeof end === 'undefined') {
     // }
-    const indexing = typeof strictIndexing === "undefined"
+    const indexing = typeof strictIndexing === 'undefined'
       ? true
       : strictIndexing;
-    const lines = typeof _lines === "string" ? [_lines] : _lines;
-    const end = typeof _end !== "undefined" ? _end : _start + 1;
+    const lines = typeof _lines === 'string' ? [_lines] : _lines;
+    const end = typeof _end !== 'undefined' ? _end : _start + 1;
 
     return this.request(`${this.prefix}set_lines`, [
       this,
       _start,
       end,
       indexing,
-      lines
+      lines,
     ]);
   }
 
   // Insert lines at `start` index
-  insert(lines, start) {
+  insert(lines: Array<string> | string, start: number) {
     return this.setLines(lines, { start, end: start, strictIndexing: true });
   }
 
   // Replace lines starting at `start` index
-  replace(_lines, start) {
-    const lines = typeof _lines === "string" ? [_lines] : _lines;
+  replace(_lines: Array<string> | string, start: number) {
+    const lines = typeof _lines === 'string' ? [_lines] : _lines;
     return this.setLines(lines, {
       start,
       end: start + lines.length + 1,
-      strictIndexing: false
+      strictIndexing: false,
     });
   }
 
-  remove(start, end, strictIndexing) {
+  remove(start: number, end: number, strictIndexing: boolean) {
     return this.setLines([], { start, end, strictIndexing });
   }
 
   // Append a string or list of lines to end of buffer
-  append(lines) {
+  append(lines: Array<string> | string) {
     return this.setLines(lines, { start: -1, end: -1, strictIndexing: false });
   }
 
@@ -111,8 +111,7 @@ export class Buffer extends BaseApi {
     return this.request(`${this.prefix}is_valid`, [this]);
   }
 
-  // @return Promise<[rol, col]>
-  mark(name): Promise<[number, number]> {
+  mark(name: string): Promise<[number, number]> {
     return this.request(`${this.prefix}get_mark`, name);
   }
 
@@ -121,43 +120,41 @@ export class Buffer extends BaseApi {
   // return Range(this, start, end)
   // }
 
-  addHighlight(
-    {
-      hlGroup,
-      line,
-      colStart,
-      colEnd,
-      srcId,
-      async: _isAsync
-    }: BufferHighlight = {
-      colStart: 0,
-      colEnd: -1,
-      srcId: -1
-    }
-  ) {
-    const isAsync = _isAsync || typeof srcId !== "undefined";
+  addHighlight({
+    hlGroup,
+    line,
+    colStart,
+    colEnd,
+    srcId,
+    async: _isAsync,
+  }: BufferHighlight) {
+    const colEnd = typeof _end !== 'undefined' ? _end : -1;
+    const colStart = typeof _start !== 'undefined' ? _start : -0;
+    const srcId = typeof _srcId !== 'undefined' ? _srcId : -1;
+    const isAsync = _isAsync || typeof srcId !== 'undefined';
     return this.request(`${this.prefix}add_highlight`, [
       srcId,
       hlGroup,
       line,
       colStart,
       colEnd,
-      isAsync
+      isAsync,
     ]);
   }
 
   clearHighlight(
     { srcId, lineStart, lineEnd, async }: BufferClearHighligh = {
+      srcId: -1,
       lineStart: 0,
       lineEnd: -1,
-      async: true
+      async: true,
     }
   ) {
     return this.request(`${this.prefix}clear_highlight`, [
       srcId,
       lineStart,
       lineEnd,
-      async
+      async,
     ]);
   }
 }
