3e8e658e4d9f30d90a905c8c4c03c164f64d22ab
Billy Vong <billyvg@gmail.com>
2017-10-29 10:36:13 -0700

    ref(logger): Clean up logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,13 @@
 import * as winston from 'winston';
 
-let logger: winston.LoggerInstance;
-
-(<any>winston).level = process.env.NVIM_NODE_LOG_LEVEL || 'debug';
-let transports = [];
+const transports = [];
+const level = process.env.NVIM_NODE_LOG_LEVEL || 'debug';
 
 if (process.env.NVIM_NODE_LOG_FILE) {
   transports.push(
     new winston.transports.File({
       filename: process.env.NVIM_NODE_LOG_FILE,
-      level: winston.level,
+      level,
       json: false,
     })
   );
@@ -19,8 +17,8 @@ if (process.env.ALLOW_CONSOLE) {
   transports.push(winston.transports.Console);
 }
 
-logger = new winston.Logger({
-  level: process.env.NVIM_NODE_LOG_LEVEL || 'debug',
+const logger: winston.LoggerInstance = new winston.Logger({
+  level,
   transports,
 });
 
