22e990ce273670d039d12923f54ccaa12310edd8
chemzqm <chemzqm@gmail.com>
2018-06-15 11:09:45 +0800

    fix(api): Export NeovimClient

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 export { Neovim } from './Neovim';
+export { NeovimClient } from './client';
 export { Buffer } from './Buffer';
 export { Window } from './Window';
 export { Tabpage } from './Tabpage';
