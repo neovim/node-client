3615a21673a665f04acfd51ab28d37af4ef3716a
Qiming zhao <chemzqm@gmail.com>
2018-06-15 13:24:21 +0800

    fix(api): Export NeovimClient (#88)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { attach } from './attach';
-export { Neovim, Buffer, Tabpage, Window } from './api/index';
+export { Neovim, NeovimClient, Buffer, Tabpage, Window } from './api/index';
 export { Plugin, Function, Autocmd, Command } from './plugin';
 export { NvimPlugin } from './host/NvimPlugin';
