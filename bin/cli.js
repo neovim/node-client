e16108a9eee858d142b62764e4f1997aaf9cc43e
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-09-30 19:54:13 -0400

    debug: spawn child process with --inspect-brk
    npm uses shims (cmd.exe, sh, node) in node_modules/.bin/
    to run a nodejs script from a npm module.
    On Ubuntu, there is 1 shim for nodejs.
    On Windows, there is 1 shim each for cmd.exe and sh.
    npm shims do not allow the user to pass arguments to nodejs
    and debugging a module requires passing arguments to nodejs
    so the user can debug a module only by running the nodejs directly
    and passing to it the filepath of the nodejs script, referred by the shim.
    Determining this filepath is not fast enough for npm and yarn
    so the client should handle this.
    
    The client spawns nodejs with '--inspect-brk' to run a child client,
    preserving all commandline arguments, under the following conditions:
    
    1. $NVIM_NODE_HOST_DEBUG is set.
    2. nodejs supports '--inspect-brk'. Check nodejs semver version.
    3. '--inspect-brk' is not passed to nodejs.
    
    Fix #46
    
    NodeJS Changelog links:
    https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V6.md#6.12.0
    https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V7.md#7.6.0
    https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V8.md#8.0.0

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-const Host = require('../lib/host').Host;
-const logger = require('../lib/utils/logger').logger;
+const { Host } = require('../lib/host');
+const { logger } = require('../lib/utils/logger');
+const { spawnSync } = require('child_process');
+const semver = require('semver');
 
 // node <current script> <rest of args>
 const [, , ...args] = process.argv;
@@ -11,6 +13,19 @@ if (args[0] === '--version') {
   // eslint-disable-next-line no-console
   console.log(pkg.version);
   process.exit(0);
+}
+
+if (
+  process.env.NVIM_NODE_HOST_DEBUG &&
+  semver.satisfies(process.version, '>=7.6.0 || >=6.12.0 <7.0.0') &&
+  process.execArgv.every(token => token !== '--inspect-brk')
+) {
+  const childHost = spawnSync(
+    process.execPath,
+    process.execArgv.concat(['--inspect-brk']).concat(process.argv.slice(1)),
+    { stdio: 'inherit' }
+  );
+  process.exit(childHost.status);
 }
 
 try {
