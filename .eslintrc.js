bc4dec6c36190abb4b981527b03489835cee2c10
Billy Vong <billy@sentry.io>
2019-03-06 14:11:23 +0700

    use eslint and typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,35 @@
 module.exports = {
   // extending prettier fucks up my vim...
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['import'],
-  overrides: {
-    files: ['**/*.ts'],
-    parser: 'typescript-eslint-parser',
-    plugins: ['typescript'],
-    rules: {
-      'no-undef': 'off',
-
-      'typescript/no-unused-vars': 1,
-
-      'prefer-destructuring': 'off',
-
-      // https://github.com/eslint/typescript-eslint-parser/issues/414
-      'no-restricted-globals': 'off',
-
-      // https://github.com/eslint/typescript-eslint-parser/issues/434
-      'no-dupe-class-members': 'off',
-
-      'no-use-before-define': 'off',
-    },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['import', 'prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
   },
+
+  // overrides: {
+  // files: ['**/*.ts'],
+  // rules: {
+  // 'no-undef': 'off',
+
+  // 'typescript/no-unused-vars': 1,
+
+  // 'prefer-destructuring': 'off',
+
+  // // https://github.com/eslint/typescript-eslint-parser/issues/414
+  // 'no-restricted-globals': 'off',
+
+  // // https://github.com/eslint/typescript-eslint-parser/issues/434
+  // 'no-dupe-class-members': 'off',
+
+  // 'no-use-before-define': 'off',
+  // },
+  // },
 
   rules: {
     camelcase: ['error', { properties: 'never' }],
@@ -43,17 +51,17 @@ module.exports = {
     'func-names': 0,
 
     // prettier things
-    'arrow-parens': 0,
-    'space-before-function-paren': 0,
+    'prettier/prettier': 'error',
+    // 'arrow-parens': 0,
+    // 'space-before-function-paren': 0,
 
     'import/extensions': 0,
     'import/prefer-default-export': 0,
+
+    '@typescript-eslint/no-explicit-any': ['off'],
   },
 
   settings: {
     'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts'] } },
-    'import/parsers': {
-      'typescript-eslint-parser': ['.ts', '.tsx'],
-    },
   },
 };
