1b4060f7c9d8d729a9a794add024dd4aca534655
Justin M. Keyes <justinkz@gmail.com>
2024-03-26 16:31:09 +0100

    WIP: eliminate "host" concept
    Problem:
    The "remote plugin" concept is too complicated. https://github.com/neovim/neovim/issues/27949
    
    Solution:
    - Let the "client" also be the "host". Eliminate the separate "host"
      concept and related modules.
    - Let any node module be a "host". Any node module that imports the
      "neovim" package and defines method handler(s) is a "remote module".
      It is loaded by Nvim same as any "node client".
    
    Story:
    - The value in rplugins is:
      1. it finds the interpreter on the system
      2. it figures out how to invoke the main script with the interpreter
    
    Old architecture:
    
        nvim rplugin framework ->
          node: cli.js ->
            starts the "plugin Host"
              attaches itself to current node process
                searches for plugins and tries to load them in the node process (MULTI-TENANCY)
                  -> plugin1
                  -> plugin2
                  -> ...
    
    New architecture:
    
        nvim vim.rplugin('node', '…/plugin1.js') ->
          node: neovim.cli()
        nvim vim.rplugin('node', '…/plugin2.js') ->
          node: neovim.cli()
    
    1. A Lua plugin calls `vim.rplugin('node', '/path/to/plugin.js')`.
    2. Each call to `vim.rplugin()` starts a new node process (no "multi-tenancy").
    3. plugin.js is just a normal javascript file that imports the `neovim` package.
    4. plugin.js provides a "main" function. It can simply import the `neovim.cli()` util function, which handles attaching/setup.
    
    TEST CASE / DEMO:
    
        const found = findNvim({ orderBy: 'desc', minVersion: '0.9.0' })
        const nvim_proc = child_process.spawn(found.matches[0].path, ['--clean', '--embed'], {});
        const nvim = attach({ proc: nvim_proc });
        nvim.setHandler('foo', (ev, args) => {
          nvim.logger.info('handled from remote module: "%s": args:%O', ev.name, args);
        });
        nvim.callFunction('rpcrequest', [(await nvim.channelId), 'foo', [42, true, 'bar']]);
    
        2024-03-26 16:47:35 INF handleRequest: foo
        2024-03-26 16:47:35 DBG request received: foo
        2024-03-26 16:47:35 INF handled from remote module: "foo": args:[ [ 42, true, 'bar' ] ]

--- packages/neovim/src/index.ts
+++ packages/neovim/src/index.ts
@@ -1,4 +1,5 @@
 export { attach } from './attach/attach';
+export { cli } from './cli';
 export { Neovim, NeovimClient, Buffer, Tabpage, Window } from './api/index';
 export { Plugin, Function, Autocmd, Command } from './plugin';
 export { NvimPlugin } from './host/NvimPlugin';
