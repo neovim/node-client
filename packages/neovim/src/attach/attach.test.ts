52e5d1ab7361d36291c07a6f0f3274e0dd4c798f
Justin M. Keyes <justinkz@gmail.com>
2024-10-17 03:51:06 -0700

    test(transport): high-traffic RPC #412
    Problem:
    In https://github.com/vscode-neovim/vscode-neovim/issues/2184#issuecomment-2369880327
    it was reported that "Github Copilot Chat opens MANY buffers while
    writing code, and that somehow breaks the vscode-neovim extension".
    
    Solution:
    - Add test coverage for high amounts of RPC traffic.
    - TODO: this doesn't cover complex scenarios such as a looping
      `OptionSet` handler.

--- packages/neovim/src/attach/attach.test.ts
+++ packages/neovim/src/attach/attach.test.ts
@@ -11,30 +11,33 @@ describe('Nvim API', () => {
   let proc: ReturnType<typeof testUtil.startNvim>[0];
   let nvim: ReturnType<typeof testUtil.startNvim>[1];
 
+  /** Incoming requests (from Nvim). */
+  const requests: { method: string; args: number[] }[] = [];
+  /** Incoming notifications (from Nvim). */
+  const notifications: { method: string; args: number[] }[] = [];
+
   before(async () => {
     [proc, nvim] = testUtil.startNvim();
+
+    // Incoming requests (from Nvim).
+    nvim.on('request', (method, args, resp) => {
+      requests.push({ method, args });
+      resp.send(`received ${method}(${args})`);
+    });
+
+    // Incoming notifications (from Nvim).
+    nvim.on('notification', (method, args) => {
+      notifications.push({ method, args });
+    });
   });
 
   after(() => {
     testUtil.stopNvim();
   });
 
-  let requests: { method: string; args: number[] }[];
-  let notifications: { method: string; args: number[] }[];
-
-  before(async () => {
-    nvim.on('request', (method, args, resp) => {
-      requests.push({ method, args });
-      resp.send(`received ${method}(${args})`);
-    });
-    nvim.on('notification', (method, args) => {
-      notifications.push({ method, args });
-    });
-  });
-
   beforeEach(() => {
-    requests = [];
-    notifications = [];
+    requests.length = 0;
+    notifications.length = 0;
   });
 
   it('failure modes', async () => {
@@ -101,6 +104,30 @@ describe('Nvim API', () => {
     testUtil.stopNvim(nvim2);
   });
 
+  it('noisy RPC traffic', async () => {
+    let requestCount = 0;
+    const oldRequest = nvim.request;
+    nvim.request = function (
+      this: any,
+      name: string,
+      args: any[] = []
+    ): Promise<any> {
+      requestCount = requestCount + 1;
+      return oldRequest.call(this, name, args);
+    };
+
+    for (let i = 0; i < 99; i = i + 1) {
+      nvim.command('noswapfile edit test-node-client.lua');
+      nvim.command('bwipeout!');
+    }
+
+    expect(requestCount).toEqual(99 * 2);
+    // Still alive?
+    expect(await nvim.eval('1+1')).toEqual(2);
+
+    nvim.request = oldRequest;
+  });
+
   it('can send requests and receive response', async () => {
     const result = await nvim.eval('{"k1": "v1", "k2": 2}');
     expect(result).toEqual({ k1: 'v1', k2: 2 });
@@ -149,7 +176,7 @@ describe('Nvim API', () => {
       end: -1,
       strictIndexing: true,
     });
-    expect(lines).toEqual([]);
+    expect(lines).toEqual(['']);
 
     buf.setLines(['line1', 'line2'], { start: 0, end: 1 });
     const newLines = await buf.getLines({
